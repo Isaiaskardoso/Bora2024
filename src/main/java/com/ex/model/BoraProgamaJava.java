9acebc6a173de5331b4afd5ec9cc1f2299b0c942
IsaiasKardoso <isaiaskardososilva@gmail.com>
2024-01-08 12:18:26 -0300

    Vamos começar!

---
+++ src/main/java/com/ex/model/BoraProgamaJava.java
@@ -0,0 +1,9 @@
+package com.ex.model;
+
+public class BoraProgamaJava {
+    public static void main(String[] args) {
+
+        String string = "Olá 2024, vamos programar!";
+        System.out.println(string);
+    }
+}
